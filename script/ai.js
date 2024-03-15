0f37761f7c90902fb608455f94134ae98cc514d2
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-03-15 22:30:23 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: "ashley",
+    name: "ai",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "Jonell Magallanes", //API BY MARK
