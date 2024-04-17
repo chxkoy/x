c103560d71905236392a15cf937aab9ccd800934
chxkoy <159108439+chxkoy@users.noreply.github.com>
2024-04-17 14:20:29 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['help'],
+  aliases: ['info'],
   description: "Beginner's guide",
   usage: "Help [page] or [command]",
   credits: 'Develeoper',
@@ -21,35 +21,34 @@ module.exports.run = async function({
     const eventCommands = enableCommands[1].handleEvent;
     const commands = enableCommands[0].commands;
     if (!input) {
-      const pages = 100;
+      const pages = 20;
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
+      let helpMessage = `• —— [ 𝗔𝗟𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 ] —— •\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}.  ${prefix}${commands[i]}\n`;
+        helpMessage += `╭─❍\n➠ ${prefix}${commands[i]}\n╰───────────⟡\n\n`;
       }
-      helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}\n\n`;
-      helpMessage += '====『𝗙𝗘𝗔𝗧𝗨𝗥𝗘 𝗟𝗜𝗦𝗧:』====\n▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n';
+      helpMessage += '\n• —— [ 𝗔𝗟𝗟 𝗘𝗩𝗘𝗡𝗧𝗦 ] —— •\n\n';
       eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `╭─────────────────╮\n |\t『 ${index + 1}.』  ${prefix}${eventCommand}\n╰─────────────────╯ \n`;
+        helpMessage += `╭─❍\n➠ ${prefix}${eventCommand}\n╰───────────⟡\n\n`;
       });
-      helpMessage += `\nTo view information about a specific command, type '${prefix}help command name'.`;
+      helpMessage += `\n› Page ${page}/${Math.ceil(commands.length / pages)}\n› To view the next page, type “${prefix}help page number” To view information about a specific command, type “${prefix}help command name”\n› Create your own bot here using appstate\n› https://x3x-v0xr.onrender.com/`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else if (!isNaN(input)) {
       const page = parseInt(input);
       const pages = 20;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
+      let helpMessage = `• —— [ 𝗔𝗟𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 ] —— •\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. 『 ${prefix}${commands[i]} 』\n`;
+        helpMessage += `╭─❍\n➠ ${prefix}${commands[i]}\n╰───────────⟡\n`;
       }
-      helpMessage += '\n𝗙𝗘𝗔𝗧𝗨𝗥𝗘 𝗟𝗜𝗦𝗧:\n\n';
+      helpMessage += '\n• —— [ 𝗔𝗟𝗟 𝗘𝗩𝗘𝗡𝗧𝗦 ] —— •\n\n';
       eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 『 ${prefix}${eventCommand} 』\n`;
+        helpMessage += `╭─❍\n➠ ${prefix}${eventCommand}\n╰───────────⟡\n`;
       });
-      helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}`;
+      helpMessage += `\n› Page ${page}/${Math.ceil(commands.length / pages)}\n› To view the next page, type “${prefix}help page number” To view information about a specific command, type “${prefix}help command name”\n› Create your own bot here using appstate\n› https://x3x-v0xr.onrender.com/`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else {
       const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
@@ -65,14 +64,14 @@ module.exports.run = async function({
           cooldown,
           hasPrefix
         } = command;
-        const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
-        const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
-        const descriptionMessage = description ? `Description: ${description}\n` : '';
-        const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
-        const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
-        const versionMessage = version ? `➛ Version: ${version}\n` : '';
-        const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
-        const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
+        const roleMessage = role !== undefined ? (role === 0 ? '╭─❍\n➠ 𝗣𝗘𝗥𝗠𝗜𝗦𝗦𝗜𝗢𝗡: user\n╰───────────⟡\n' : (role === 1 ? '╭─❍\n➠ 𝗣𝗘𝗥𝗠𝗜𝗦𝗦𝗜𝗢𝗡: admin\n╰───────────⟡\n' : (role === 2 ? '╭─❍\n➠ 𝗣𝗘𝗥𝗠𝗜𝗦𝗦𝗜𝗢𝗡: thread Admin\n╰───────────⟡\n' : (role === 3 ? '╭─❍\n➠ 𝗣𝗘𝗥𝗠𝗜𝗦𝗦𝗜𝗢𝗡: super Admin\n╰───────────⟡\n' : '')))) : '';
+        const aliasesMessage = aliases.length ? `╭─❍\n➠ 𝗔𝗟𝗜𝗔𝗦𝗘𝗦: ${aliases.join(', ')}\n╰───────────⟡\n` : '';
+        const descriptionMessage = description ? `╭─❍\n➠ 𝗗𝗘𝗦𝗖: ${description}\n╰───────────⟡\n` : '';
+        const usageMessage = usage ? `╭─❍\n➠ 𝗨𝗦𝗔𝗚𝗘: ${usage}\n╰───────────⟡\n` : '';
+        const creditsMessage = credits ? `╭─❍\n➠ 𝗖𝗥𝗘𝗗𝗜𝗧𝗦: ${credits}\n╰───────────⟡\n` : '';
+        const versionMessage = version ? `╭─❍\n➠ 𝗩𝗘𝗥𝗦𝗜𝗢𝗡: ${version}\n╰───────────⟡\n` : '';
+        const cooldownMessage = cooldown ? `╭─❍\n➠ 𝗖𝗢𝗢𝗟 𝗗𝗢𝗪𝗡: ${cooldown} second(s)\n╰───────────⟡\n` : '';
+        const message = `╭─❍\n➠ 𝗡𝗔𝗠𝗘: ${name}\n╰───────────⟡\n${versionMessage}${roleMessage}${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
         api.sendMessage(message, event.threadID, event.messageID);
       } else {
         api.sendMessage('Command not found.', event.threadID, event.messageID);
@@ -92,7 +91,7 @@ module.exports.handleEvent = async function({
     messageID,
     body
   } = event;
-  const message = prefix ? 'This is my prefix: ' + prefix : "Sorry i don't have prefix";
+  const message = prefix ? 'this is my prefix : ' + prefix : "╭─❍\n➠ Sorry i don't have prefix\n╰───────────⟡";
   if (body?.toLowerCase().startsWith('prefix')) {
     api.sendMessage(message, threadID, messageID);
   }
